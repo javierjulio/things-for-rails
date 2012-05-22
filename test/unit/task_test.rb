1d281e2f549dd67db751ae0d894c999d6340445b
Javier Julio <jjfutbol@gmail.com>
2012-05-21 21:13:23 -0400

    Update task test to use FactoryGirl for creating tasks.

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -5,6 +5,10 @@ class TaskTest < ActiveSupport::TestCase
   should validate_presence_of(:title)
   
   test "should return all tasks due today" do
+    FactoryGirl.create(:task)
+    FactoryGirl.create(:task_due_today)
+    FactoryGirl.create(:task_overdue)
+    
     tasks = Task.due_today
     
     tasks.each do |t|
@@ -13,6 +17,11 @@ class TaskTest < ActiveSupport::TestCase
   end
   
   test "should return all tasks due today and overdue" do
+    FactoryGirl.create(:task)
+    FactoryGirl.create(:task_due_today)
+    FactoryGirl.create(:task_overdue)
+    FactoryGirl.create(:task_due_tomorrow)
+    
     tasks = Task.due_till_today
     
     tasks.each do |t|
@@ -21,9 +30,13 @@ class TaskTest < ActiveSupport::TestCase
   end
   
   test "should return all tasks tagged with all specified tags" do
-    tags = [tags(:personal), tags(:home)]
+    FactoryGirl.create(:task) # default has no tags
+    task = FactoryGirl.create(:task_with_tags)
     
-    tasks = Task.tagged_with_any tags(:personal), tags(:home)
+    tags = task.tags
+    
+    tasks = Task.tagged_with_any tags
+    
     
     tasks.each do |t|
       assert_equal t.tags.map(&:id).sort, tags.map(&:id).sort
